605a86a2208ccec054836dc864bf647e4cfcf66e
MariyaPuzyr <mariyapyzur@gmail.com>
2020-08-25 13:53:19 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+"use strict";
+const concat = require('gulp-concat');
+global.$ = {
+  path: {
+    task: require('./gulp/path/tasks.js')
+  },
+  gulp: require('gulp'),
+  browserSync: require('browser-sync').create(),
+  del: require('del')
+};
+
+$.path.task.forEach(function (taskPath) {
+  require(taskPath)();
+});
+$.gulp.task('main-libs', function () {
+  return $.gulp.src([
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/slick-carousel/slick/slick.min.js',
+    './node_modules/jquery-form-styler/dist/jquery.formstyler.js'
+  ])
+      .pipe(concat('main-libs.min.js'))
+      .pipe($.gulp.dest('./dist/js/'))
+
+});
+$.gulp.task('dev', $.gulp.series(
+    'clean',
+    $.gulp.parallel(
+        'pug',
+        'fonts',
+        'styles:dev',
+        'img:dev',
+        'js:dev',
+        'svg',
+        'main-libs'
+    )
+));
+
+$.gulp.task('default', $.gulp.series('dev', 'watch', 'serve-new'));
