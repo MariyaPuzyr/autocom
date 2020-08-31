404ce7a954e922d6b1835182810cdc26ff4b74f9
MariyaPuzyr <mariyapyzur@gmail.com>
2020-08-31 13:45:25 +0300

    add new modal services

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ $.path.task.forEach(function (taskPath) {
 $.gulp.task('main-libs', function () {
   return $.gulp.src([
     './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/bootstrap/dist/js/bootstrap.min.js',
     './node_modules/slick-carousel/slick/slick.min.js',
     './node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js',
     // './src/js/jquery-ui.min.js'
